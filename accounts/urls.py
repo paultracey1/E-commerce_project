5e999d28e21dc85a5632aff75cc35a358c2f5c91
Paul Tracey <Paul Tracey>
2017-06-27 15:18:22 +0100

    log in working

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import logout, profile
+from .views import logout, profile, login
 
 urlpatterns = [
     url(r'^profile/$', profile, name='profile'),
     url(r'^logout/$', logout, name='logout'),
+    url(r'^login', login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from accounts.forms import UserLoginForm
 from django.contrib import messages, auth
 from django.core.urlresolvers import reverse
+from django.template.context_processors import csrf
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -8,6 +10,31 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/accounts/login')
 def profile(request):
     return render(request, 'profile.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(username=request.POST.get('username_or_email'),
+                                     password=request.POST.get('password'))
+
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+
+                if request.GET and 'next' in request.GET:
+                    next = request.GET['next']
+                    return HttpResponseRedirect(next)
+                else:
+                    return redirect(reverse('profile'))
+            else:
+                form.add_error(None, "Your username or password was not recognised")
+    else:
+        form = UserLoginForm()
+
+    args = {'form': form, 'next': request.GET['next'] if request.GET and 'next' in request.GET else ''}
+    args.update(csrf(request))
+    return render(request, 'login.html', args)
 
 def logout(request):
     auth.logout(request)
